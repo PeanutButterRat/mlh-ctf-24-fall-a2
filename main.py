378c5ba72e7bcf3f9f8448937e0f5500e4e6fafa
Eric Brown <ebrown5676@gmail.com>
2024-11-12 15:33:40 -0800

    Add flag check

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+# TODO: Remember to remove this line before committing!
+encrypted = [0x25, 0x26, 0x22, 0xf, 0x28, 0x5e, 0x2e, 0x33, 0x27, 0x5a, 0x25, 0x25, 0x59, 0x1e, 0x15]
+
+
 def main():
-    print('Hello, world!')
+    guess = input("Enter your guess for the flag: ")
+
+    if check(guess):
+        print("That is correct! Well done!")
+    else:
+        print("Sorry, keep trying!")
 
 
-if __name__ == '__main__':
+def check(guess):
+    if len(guess) != len(encrypted):
+        return False
+
+    for i, c in enumerate(guess):
+        if (ord(c) ^ 123) + len(guess) != encrypted[i]:
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
     main()
